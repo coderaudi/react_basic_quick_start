6da7a1cd4f5d6736576d643c4d7e0361d0cbbae7
abhijeetkhire@gmail.com <abhijeetkhire@gmail.com>
2019-12-13 00:48:21 +0530

    contact us message

--- src/routes/contactus.js
+++ src/routes/contactus.js
@@ -1,11 +1,56 @@
 import React, { Component } from 'react';
 
 class Contactus extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            userMessage: "",
+            isSubmitted: false
+        }
+        this.isSubmitted = false
+    }
+
+    onChangeUserMessage = (e) => {
+        this.setState({
+            userMessage: e.target.value
+        })
+    }
+
+    submitMessage = (e) => {
+        this.setState({
+            isSubmitted: true
+        })
+    }
+    deleteMessage = (e) => {
+        this.setState({
+            isSubmitted: false,
+            userMessage: ""
+        })
+    }
+
+
     render() {
         return (
-            <div>
+            <div style={{ marginLeft: "200px" }}>
                 <h3>Contact US</h3>
+                <input type="text"
+                    value={this.state.userMessage}
+                    onChange={e => this.onChangeUserMessage(e)} />
+                <br />
+                <button onClick={e => this.submitMessage(e)}>Submit Me</button>
 
+                {this.state.isSubmitted ? <div>
+
+                    <hr />
+                    Your Message : {this.state.userMessage}
+
+                    <br />
+
+                    <button onClick={e => this.deleteMessage(e)}>Delete Your Message</button>
+                </div> : ""
+                }
             </div>
         );
     }
